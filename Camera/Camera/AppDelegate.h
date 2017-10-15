a549564084fde7308209ae3caebe470c86e5b640
王建华 <wangjianhuasuperman@icloud.com>
2017-10-15 11:13:02 +0800

    this App will begin, by SuperMan.

---
+++ Camera/Camera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by 王建华 on 2017/10/15.
+//  Copyright © 2017年 superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Camera/Camera/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Camera
+//
+//  Created by 王建华 on 2017/10/15.
+//  Copyright © 2017年 superman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"this is test Camera App. by SuperMan.");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
